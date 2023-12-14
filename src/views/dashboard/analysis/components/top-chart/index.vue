c690e03a662315eff8825016dcfa030d0567b69b
raokun <272728816@qq.com>
2023-12-14 23:56:25 +0800

    feat(projects): 数据看板功能更新

--- src/views/dashboard/analysis/components/top-chart/index.vue
+++ src/views/dashboard/analysis/components/top-chart/index.vue
@@ -2,18 +2,17 @@
   <n-grid :x-gap="16" :y-gap="16" :item-responsive="true">
     <n-grid-item span="0:24 640:24 1024:8">
       <n-card :bordered="false" class="rounded-16px shadow-sm">
+        <n-select class="w-120px ml-auto" v-model:value="saleSelectType" :options="pieModelOptions" @update:value="getSaleAnalysis"/>
         <div class="w-full h-360px py-12px">
           <h3 class="text-16px font-bold">销售统计</h3>
-          <p class="text-#aaa">统计最近一个月数据</p>
-          <h3 class="pt-32px text-24px font-bold">
-            <count-to prefix="$" :start-value="0" :end-value="7754" />
-          </h3>
-          <p class="text-#aaa">本月销售额</p>
-          <h3 class="pt-32px text-24px font-bold">
-            <count-to :start-value="0" :end-value="1234" />
-          </h3>
-          <p class="text-#aaa">本月订单数</p>
-          <n-button class="mt-24px whitespace-pre-wrap" type="primary">Last Month Summary</n-button>
+          <p class="text-#aaa">统计数据</p>
+          <div v-for="item in saleData">
+            <h3 class="pt-32px text-24px font-bold">
+              <count-to v-if="item.key =='销售额'" prefix="$" :start-value="0" :end-value="item.total" />
+              <count-to v-else  :start-value="0" :end-value="item.total" />
+            </h3>
+            <p class="text-#aaa" >{{ item.key }}</p>
+          </div>
         </div>
       </n-card>
     </n-grid-item>
@@ -25,6 +24,10 @@
     </n-grid-item>
     <n-grid-item span="0:24 640:24 1024:8">
       <n-card :bordered="false" class="rounded-16px shadow-sm">
+        <div style="display: flex; justify-content: space-between;">
+          <n-select class="w-120px" v-model:value="analysisSelectType" :options="analysisTypeOptions" @update:value="getModelList"/>
+          <n-select class="w-120px" v-model:value="modelSelectType" :options="pieModelOptions" @update:value="getModelList"/>
+        </div>
         <div ref="pieRef" class="w-full h-360px"></div>
       </n-card>
     </n-grid-item>
@@ -40,6 +43,12 @@
         <div ref="useRef" class="w-full h-360px"></div>
       </n-card>
     </n-grid-item>
+    <n-grid-item span="0:24 640:24 1024:8">
+      <n-card :bordered="false" class="rounded-16px shadow-sm">
+        <n-select class="w-120px ml-auto" v-model:value="saleMoneySelectType" :options="Options" @update:value="getSaleMoneyList"/>
+        <div ref="saleMoneyRef" class="w-full h-360px"></div>
+      </n-card>
+    </n-grid-item>
   </n-grid>
 </template>
 
@@ -47,23 +56,43 @@
 import {onMounted, ref, watchEffect} from 'vue';
 import type { Ref } from 'vue';
 import { type ECOption, useEcharts } from '@/composables';
-import {fetchAllAnalysisList, fetchAnalysisList} from '@/service';
+import {fetchAllAnalysisList, fetchAnalysisList, fetchPieAnalysisList, fetchSaleAnalysis} from '@/service';
 import AllAnalysis = ApiAnalysisManagement.AllAnalysis;
 import TotalAnalysis = ApiAnalysisManagement.TotalAnalysis;
 
 const askSelectType=ref(1);
 const tokenSelectType=ref(1);
 const useSelectType=ref(1);
+const saleMoneySelectType=ref(1);
+const modelSelectType=ref(1);
+const analysisSelectType=ref(6);
+const saleSelectType=ref(1);
 const Options: { label: string; value: number }[] = [
   { label: '当天', value: 1 },
   { label: '按月',value: 4 },
   { label: '按天', value: 0 },
 ]
+const pieModelOptions: { label: string; value: number }[] = [
+  { label: '当天', value: 1 },
+  { label: '本月',value: 4 },
+  { label: '今年', value: 5 },
+  { label: '至今', value: 0 },
+]
+
+const analysisTypeOptions: { label: string; value: number }[] = [
+  { label: '提问次数', value: 0 },
+  { label: '图片生成数',value: 2 },
+  { label: 'Token消耗量', value: 6 },
+]
+
 
 defineOptions({ name: 'DashboardAnalysisTopCard' });
 const analysisData=ref<AllAnalysis[]>([]);
 const tokenData=ref<TotalAnalysis[]>([]);
 const useData=ref<TotalAnalysis[]>([]);
+const saleMoneyData=ref<TotalAnalysis[]>([]);
+const pieData=ref<TotalAnalysis[]>([]);
+const saleData=ref<TotalAnalysis[]>([]);
 const lineOptions = ref<ECOption>({
   tooltip: {
     trigger: 'axis',
@@ -236,6 +265,44 @@ const useOptions = ref<ECOption>({
   ]
 }) as Ref<ECOption>;
 const { domRef: useRef } = useEcharts(useOptions);
+const saleMoneyOptions = ref<ECOption>({
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      label: {
+        backgroundColor: '#6a7985'
+      }
+    }
+  },
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  legend: {
+    data: ['销售额']
+  },
+  series: [
+    {
+      name: '销售额',
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: 'bar',
+      color: '#8378ea',
+      showBackground: true,
+      barGap: 100,
+      itemStyle: {
+        borderRadius: [40, 40, 0, 0]
+      },
+      backgroundStyle: {
+        color: 'rgba(180, 180, 180, 0.2)'
+      }
+    }
+  ]
+}) as Ref<ECOption>;
+const { domRef: saleMoneyRef } = useEcharts(saleMoneyOptions);
 
 const pieOptions = ref<ECOption>({
   tooltip: {
@@ -251,7 +318,7 @@ const pieOptions = ref<ECOption>({
   series: [
     {
       color: ['#5da8ff', '#8e9dff', '#fedc69', '#26deca'],
-      name: '时间安排',
+      name: '模型统计',
       type: 'pie',
       radius: ['45%', '75%'],
       avoidLabelOverlap: false,
@@ -306,6 +373,23 @@ watchEffect(() => {
   if (useOptions.value.series && Array.isArray(useOptions.value.series) && useOptions.value.series.length > 0) {
     useOptions.value.series[0].data = useData.value.map(m => m.total);
   }
+  //saleMoney
+  if (saleMoneyOptions.value.xAxis) {
+    (<any>saleMoneyOptions.value.xAxis).data = saleMoneyData.value.map(m => m.key);
+  }
+  if (saleMoneyOptions.value.series && Array.isArray(saleMoneyOptions.value.series) && saleMoneyOptions.value.series.length > 0) {
+    saleMoneyOptions.value.series[0].data = saleMoneyData.value.map(m => m.total);
+  }
+  //pie
+  if (pieOptions.value.xAxis) {
+    (<any>pieOptions.value.xAxis).data = pieData.value.map(m => m.key);
+  }
+  if (pieOptions.value.series && Array.isArray(pieOptions.value.series) && pieOptions.value.series.length > 0) {
+    pieOptions.value.series[0].data = pieData.value.map(item => ({
+      value: item.total,
+      name: item.key,
+    }));
+  }
 });
 async function getAnalysisList() {
 	const { data } = await fetchAllAnalysisList(askSelectType.value, null, null);
@@ -316,13 +400,31 @@ async function getAnalysisList() {
 async function getTokenList() {
   const { data } = await fetchAnalysisList(tokenSelectType.value, null, null,6);
   if (data) {
-    useData.value = data;
+    tokenData.value = data;
   }
 }
 async function getUseList() {
-  const { data } = await fetchAnalysisList(tokenSelectType.value, null, null,1);
+  const { data } = await fetchAnalysisList(useSelectType.value, null, null,1);
   if (data) {
-    tokenData.value = data;
+    useData.value = data;
+  }
+}
+async function getSaleMoneyList() {
+  const { data } = await fetchAnalysisList(saleMoneySelectType.value, null, null,5);
+  if (data) {
+    saleMoneyData.value = data;
+  }
+}
+async function getModelList() {
+  const { data } = await fetchPieAnalysisList(modelSelectType.value, null, null,analysisSelectType.value);
+  if (data) {
+    pieData.value = data;
+  }
+}
+async function getSaleAnalysis() {
+  const { data } = await fetchSaleAnalysis(null, null, null,analysisSelectType.value);
+  if (data) {
+    saleData.value = data;
   }
 }
 async function askHandleUpdateValue(){
@@ -332,7 +434,10 @@ async function askHandleUpdateValue(){
 onMounted(() => {
   setTimeout(() => { getAnalysisList(); }, 1000);
   getTokenList();
-  getUseList();
+   getUseList();
+  getSaleMoneyList();
+  getModelList();
+  getSaleAnalysis();
 });
 </script>
 
